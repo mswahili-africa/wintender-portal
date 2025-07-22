5f0412db6e7799fbce0c08b12a5592dd5c348136
Johannes Mwasanyila <joachriss@gmail.com>
2025-07-22 17:05:16 +0300

    Removed manager access from edit bidder details button

--- src/pages/bidders/fragments/bidderProfileModal.tsx
+++ src/pages/bidders/fragments/bidderProfileModal.tsx
@@ -233,7 +233,7 @@ const BidderProfileModal: React.FC<IProps> = ({ user, onClose }) => {
                                     {/* JCM edit button */}
                                     <div className="flex justify-end w-full">
                                         {
-                                            ["MANAGER","SUPERVISOR"].includes(userData?.role || "") && // Show edit button only for supervisors
+                                            ["SUPERVISOR"].includes(userData?.role || "") && // Show edit button only for supervisors
                                             <Button
                                                 label={editDetails ? "Cancel Edit" : "Edit Details"}
                                                 size="sm"
